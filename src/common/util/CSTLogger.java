553708cb21ccc40ae86fc9920c72d9d3947d7620
brzone <brzone@126.com>
2014-06-16 15:25:50 +0800

    使用clazz名字
    Signed-off-by: brzone <brzone@126.com>

--- src/common/util/CSTLogger.java
+++ src/common/util/CSTLogger.java
@@ -1,6 +1,5 @@
 package common.util;
 
-import org.slf4j.LoggerFactory;
 
 public class CSTLogger {
     
@@ -16,19 +15,19 @@ public class CSTLogger {
 	}
 
 	public void logError(String fmt, final Object... inParams) {
-		printf(fmt + "," + inParams);
+		printf(clazz.getName() + ":" +fmt + "," + inParams);
 	}
 
 	public void logWarn(String fmt, final Object... inParams) {
-		printf(fmt + "," + inParams);
+		printf(clazz.getName() + ":" +fmt + "," + inParams);
 	}
 
 	public void logInfo(String fmt, final Object... inParams) {
-		printf(fmt + "," + inParams);
+		printf(clazz.getName() + ":" +fmt + "," + inParams);
 	}
 
 	public void logDebug(String fmt, final Object... inParams) {
-		printf(fmt + "," + inParams);
+		printf(clazz.getName() + ":" +fmt + "," + inParams);
 	}
 
 	public void logError(Exception e) {
